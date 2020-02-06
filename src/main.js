6ad77efcc1ed24b394f9cf62b65866942acc4d4c
linhao.fan <haohaoangle@163.com>
2020-02-06 23:17:29 +0800

    install iview & i18n

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import ViewUI from 'view-design';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import 'view-design/dist/styles/iview.css';
+
+Vue.config.productionTip = false;
+
+Vue.use(ViewUI);
 
 new Vue({
   render: h => h(App),
