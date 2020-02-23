7cc982554810b064e744f7f68c986437e3065b2e
linhao.fan <haohaoangle@163.com>
2020-02-23 23:25:04 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,15 +1,31 @@
 import Vue from 'vue';
 import ViewUI from 'view-design';
 import App from './App.vue';
-import i18n from './language';
+import VueI18n from 'vue-i18n';
 
 import 'view-design/dist/styles/iview.css';
 
+window.Vue = Vue // 要在vue-i18n实例化之前执行
+
+const i18n = new VueI18n({
+  locale: 'en',
+  messages: {
+    en: {
+      message: {
+        hello: 'hello world'
+      }
+    },
+    ja: {
+      message: {
+        hello: 'こんにちは、世界'
+      }
+    }
+  }
+})
+
 Vue.config.productionTip = false;
 
-Vue.use(ViewUI, {
-  i18n: (key, value) => i18n.t(key, value)
-});
+Vue.use(ViewUI);
 
 new Vue({
   i18n,
